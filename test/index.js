b61525caf58bd67d0deffe55bf252e5a6572e24d
Masahide Isaka <a1132666@gmail.com>
2018-08-07 14:02:53 +0900

    Fix: test code

--- test/index.js
+++ test/index.js
@@ -4,10 +4,10 @@ const fs = require('fs-extra');
 
 test('parse - case 001', (t) => {
   return styleguideLite({
-    src: './test/expect/case-001/style.css',
+    src: './test/fixtures/case-001/style.css',
     dest: './dest/assets/styleguide/',
-    template: './test/expect/case-001/index.ejs'
+    template: './test/fixtures/case-001/index.ejs'
   }).then(() => {
-    t.is(fs.readFileSync('./test/fixtures/case-001/index.html', 'UTF-8'), fs.readFileSync('./dest/assets/styleguide/index.html', 'UTF-8'));
+    t.is(fs.readFileSync('./test/expect/case-001/index.html', 'UTF-8'), fs.readFileSync('./dest/assets/styleguide/index.html', 'UTF-8'));
   });
 });
